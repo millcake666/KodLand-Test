548cdc2383f01a364c2d8e266ec6b4020e15a44a
Dmitry Gryaznov <53191191+millcake666@users.noreply.github.com>
2024-01-24 22:54:13 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import pgzrun
+import random
+
+
+WIDTH = 1200  # Ширина окна
+HEIGHT = 600  # Высота окна
+TITLE = 'Dungeon game'  # Заголовок окна игры
+FPS = 60  # Количество кадров в секунду
+
+mode = 'game'  # game / menu
+
+
+class Alien(Actor):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def animate(self):
+        pass
+
+
+class Enemy(Actor):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def animate(self):
+        pass
+
+
+def draw_map():
+    pass
+
+
+def draw():
+    pass
+
+
+def update():
+    pass
+
+
+pgzrun.go()
